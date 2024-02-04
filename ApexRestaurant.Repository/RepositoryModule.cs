8e8c6faa87f8b069ece9c7c4ab7caf71aac6807f
rushma.singh <rushma.singh@info.com.np>
2024-02-04 10:18:04 +0545

    added api controller for customer

--- ApexRestaurant.Repository/RepositoryModule.cs
+++ ApexRestaurant.Repository/RepositoryModule.cs
@@ -6,12 +6,11 @@ namespace ApexRestaurant.Repository;
 
 public static class RepositoryModule
 {
-    public static void Register(IServiceCollection services, string connection,
-    string migrationsAssembly)
-    {
-        services.AddDbContext<RestaurantContext>(options =>
-      options.UseSqlite(connection, builder =>
-      builder.MigrationsAssembly(migrationsAssembly)));
-        services.AddTransient<ICustomerRepository, CustomerRepository>();
-    }
+  public static void Register(IServiceCollection services, string connection, string migrationsAssembly)
+  {
+    services.AddDbContext<RestaurantContext>(options =>
+  options.UseSqlite(connection, builder =>
+  builder.MigrationsAssembly(migrationsAssembly)));
+    services.AddTransient<ICustomerRepository, CustomerRepository>();
+  }
 }
